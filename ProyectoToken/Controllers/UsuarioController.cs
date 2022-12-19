b89f174e165a6318f9eff075c8cf42d7bbe79bce
Facundo Villalobo <maxijul_14@hotmail.com>
2022-12-19 20:39:49 -0300

    Add Controladores para hacer las peticiones http

---
+++ ProyectoToken/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyectoToken.Models.Custom;
+using ProyectoToken.Services;
+
+
+namespace ProyectoToken.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class UsuarioController : ControllerBase
+  {
+    private readonly IAuthorizationService _authorizationService;
+
+    public UsuarioController(IAuthorizationService authorizationService)
+    {
+      _authorizationService = authorizationService;
+    }
+
+
+    [HttpPost]
+    [Route("Autenticar")]
+    // Aqui autenticamos si el parametro que recibimos del body es un token correcto y devolvemos segun lo que corresponda
+    public async Task<IActionResult> Autenticar([FromBody] AuthorizationRequest autorizacion)
+    {
+      var resultadoAutorizacion = await _authorizationService.DevolverToken(autorizacion);
+
+      if (resultadoAutorizacion == null)
+        return Unauthorized();
+
+      return Ok(resultadoAutorizacion);
+      
+    }
+
+  }
+}
